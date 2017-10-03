7506e13c2e24d56c37acad90de251dfd5ea262ea
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 15:05:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (i = 0; i < musicians.length; i++) {
     for (j = 0; j < instruments.length; j++) {
-      array.push(musicians[i] + " plays " + instruments[j]); 
+      array.push(musicians[i] + " plays " + instruments[j]);
     }
   }
   return array; 
